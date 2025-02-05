30132dd203dc52e74a8805c35b501680c37aee70
sujalmhrzn <sujal.mhrzn7@outlook.com>
2025-02-05 13:09:41 +0545

    Initialized landkit repo

---
+++ Project/src/Message.tsx
@@ -0,0 +1,10 @@
+//pascalcasing
+function Message(){
+
+const name='Sujal';
+if (name)
+ return <h1>Hello {name}</h1>
+return <h1>hello world</h1>
+}
+
+export default Message;
---
+++ Project/src/components/navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <a href="#" className="logo">
+        Landkit.
+      </a>
+      <ul className="nav-links">
+        <li>
+          <a href="#">Landings</a>
+        </li>
+        <li>
+          <a href="#">Pages</a>
+        </li>
+        <li>
+          <a href="#">Account</a>
+        </li>
+        <li>
+          <a href="#">Documentation</a>
+        </li>
+      </ul>
+      <a href="#" className="buy-btn">
+        Buy now
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
