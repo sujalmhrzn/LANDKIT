197531ae0fce159aa4d38a91f4deefdb9dba18da
sujalmhrzn <sujal.mhrzn7@outlook.com>
2025-02-07 14:45:04 +0545

    Initialized navbar,home

--- Project/src/App.tsx
+++ Project/src/App.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Buy from "./components/linkto/buy";
+import Hero from "./components/home";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/buy" element={<Buy />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Hero />
+        <Routes>
+          <Route path="/buy" element={<Buy />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- Project/src/main.tsx
+++ Project/src/main.tsx
@@ -1,5 +1,4 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { ThemeProvider } from "next-themes";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
